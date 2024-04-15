8186eb2f1bd101c7207901825ad4b0b1a85a1d7c
etisha-dudhat <etishadudhat@gmail.com>
2024-04-15 16:19:55 +0530

    first commit

---
+++ src/Example.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Example2 from './Example2'
+
+const Example = () => {
+  return (
+    <div>
+      <h1>Example of react js</h1>
+      <Example2 />
+    </div>
+  )
+}
+
+export default Example
---
+++ src/Example2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Example2 = () => {
+  return (
+    <div>
+      <p>Example 2</p>
+    </div>
+  )
+}
+
+export default Example2
